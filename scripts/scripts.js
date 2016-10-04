0d8a500aa52be7eaaaea3bef6d5cf9e0de2f0f32
Ginger Blumenthal <ginbutterfly@gmail.com>
2016-10-03 23:08:45 -0400

    add scripts folder

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+
+	$( "p" ).hover(function() {
+	  		$( this ).css( "color", "#8D909B" );
+	  	}, function() {
+	  		$( this ).css( "color", "#2d4654" );
+	});
+
+});
