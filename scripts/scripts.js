8c693f972591e744886b035bc7289e26176b787c
Ginger Blumenthal <ginbutterfly@gmail.com>
2016-10-05 23:00:10 -0400

    add new main page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,4 +7,43 @@ $(document).ready(function(){
 	  		$( this ).css( "color", "#2d4654" );
 	});
 
+	$( "#welcome" ).hover(function() {
+	  		$( this ).css( {
+	  			"border-style": "solid",
+	  			"border-color": "#eaf2ef",
+	  			 });
+	  	}, function() {
+	  		$( this ).css( "border-style", "none" );
+	});
+
+
+	$( "#student" ).hover(function() {
+	  		$( this ).css( {
+	  			"border-style": "solid",
+	  			"border-color": "#87bcde",
+	  			 });
+	  	}, function() {
+	  		$( this ).css( "border-style", "none" );
+	});
+
+	$( "#see-above" ).hover(function() {
+	  		$( this ).css( {
+	  			"border-style": "solid",
+	  			"border-color": "#eaf2ef",
+	  			 });
+	  	}, function() {
+	  		$( this ).css( "border-style", "none" );
+	});
+
+
+	$( "#contact" ).hover(function() {
+	  		$( this ).css( {
+	  			"border-style": "solid",
+	  			"border-color": "#87bcde",
+	  			 });
+	  	}, function() {
+	  		$( this ).css( "border-style", "none" );
+	});
+	
 });
+
